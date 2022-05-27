ba9216a0e1bec9224b6fde07b191e01f0346a24b
Felipe Ferreira dos Santos Cruz <fcruz@pcugroup.com>
2022-05-26 22:19:18 -0300

    Alterando o modelo do arquivo para curriculo

--- report/src/main/java/report/example/report/ReportPdf.java
+++ report/src/main/java/report/example/report/ReportPdf.java
@@ -9,6 +9,7 @@ import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.apache.pdfbox.util.Matrix;
 
 import org.vandeseer.easytable.TableDrawer;
+import org.vandeseer.easytable.settings.BorderStyleInterface;
 import org.vandeseer.easytable.settings.HorizontalAlignment;
 import org.vandeseer.easytable.settings.VerticalAlignment;
 import org.vandeseer.easytable.structure.Row;
@@ -24,7 +25,10 @@ public class ReportPdf {
         example taken from:
         http://www.ibdd.org.br/relatorios.asp
     */
-    private static final Color LIGHT_BLUE_3 = new Color(231, 241, 243);
+    private static final Color LIGHT_BLACK_1 = new Color(59, 55, 61);
+
+    private static final Color TESTER = new Color(232, 183, 215);
+
 
 
     public void generate() {
@@ -46,26 +50,73 @@ public class ReportPdf {
 
         PDPageContentStream content = new PDPageContentStream(doc, page);
 
+        PDImageXObject pdImage = PDImageXObject.createFromFile("inputPhoto.png", doc);
+        content.drawImage(pdImage, 0f, 661.10f, 200, 180);
+
         TableDrawer tableTitle = TableDrawer.builder()
-            .table(createBoxTitle("RELATÓRIO ANUAL 2019"))
+            .table(createBoxTitle(""))
+            .startX(15)
+            .startY(660.5f)
+            .build();
+        tableTitle.draw(() -> doc, () -> new PDPage(), 72f);
+
+        TableDrawer tableTitle_2 = TableDrawer.builder()
+            .table(profileBox(""))
+            .startX(15)
+            .startY(550.5f)
+            .build();
+        tableTitle_2.draw(() -> doc, () -> new PDPage(), 72f);
+
+        content.setNonStrokingColor(LIGHT_BLACK_1);
+		content.addRect(0, 0, 200, 660.5f);
+		content.fill();
+
+
+        /*TableDrawer tableTitle1 = TableDrawer.builder()
+            .table(createBoxTitle("RELATÓRIO ANUAL 2022"))
             .startX(20)
             .startY(800)
             .endY(40f)
             .build();
-        tableTitle.draw(() -> doc, () -> new PDPage(), 72f);
+        tableTitle1.draw(() -> doc, () -> new PDPage(), 72f);*/
 
         content.close();
     }
 
     private Table createBoxTitle(String title) {
-		final TableBuilder tableBuilder = Table.builder().addColumnsOfWidth(600).fontSize(8)
-			.textColor(Color.BLACK).font(PDType1Font.HELVETICA).backgroundColor(LIGHT_BLUE_3)
-			.verticalAlignment(VerticalAlignment.MIDDLE);
+		final TableBuilder tableBuilder = Table.builder().addColumnsOfWidth(170).fontSize(8)
+			.textColor(Color.BLACK).font(PDType1Font.HELVETICA).backgroundColor(TESTER)
+            .verticalAlignment(VerticalAlignment.MIDDLE);
 
 		tableBuilder.addRow(Row.builder()
-			.add(TextCell.builder().text(title).build())
-				.textColor(Color.BLACK).font(PDType1Font.HELVETICA_BOLD).fontSize(23)
-				.horizontalAlignment(HorizontalAlignment.LEFT).build());
+			.add(TextCell.builder().text("Felipe").build())
+				.textColor(Color.WHITE).font(PDType1Font.HELVETICA_BOLD).fontSize(20)
+				.horizontalAlignment(HorizontalAlignment.CENTER).build());
+        tableBuilder.addRow(Row.builder()
+			.add(TextCell.builder().text("Ferreira").build())
+				.textColor(Color.WHITE).font(PDType1Font.HELVETICA_BOLD).fontSize(20)
+				.horizontalAlignment(HorizontalAlignment.CENTER).build());
+        tableBuilder.addRow(Row.builder()
+            .add(TextCell.builder().text("Desenvolvedor FULL TALO").build())
+                .textColor(Color.WHITE).font(PDType1Font.HELVETICA_BOLD).fontSize(12)
+                .horizontalAlignment(HorizontalAlignment.CENTER).build());
+
+		return tableBuilder.build();
+	}
+
+    private Table profileBox(String title) {
+		final TableBuilder tableBuilder = Table.builder().addColumnsOfWidth(170).fontSize(8)
+			.textColor(Color.BLACK).font(PDType1Font.HELVETICA).backgroundColor(TESTER)
+            .verticalAlignment(VerticalAlignment.MIDDLE);
+
+		tableBuilder.addRow(Row.builder()
+			.add(TextCell.builder().text("Felipe").build())
+				.textColor(Color.WHITE).font(PDType1Font.HELVETICA_BOLD).fontSize(20)
+				.horizontalAlignment(HorizontalAlignment.CENTER).build());
+        tableBuilder.addRow(Row.builder()
+			.add(TextCell.builder().text("Ferreira").build())
+				.textColor(Color.WHITE).font(PDType1Font.HELVETICA_BOLD).fontSize(20)
+				.horizontalAlignment(HorizontalAlignment.CENTER).build());
 
 		return tableBuilder.build();
 	}
