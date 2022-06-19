3c0b092f395834201609eba60e28393535754f74
Felipe Ferreira dos Santos Cruz <fcruz@pcugroup.com>
2022-06-18 21:07:48 -0300

    Primeira versao da tela do sistema

--- report-web/js.js
+++ report-web/js.js
@@ -1,6 +1,8 @@
 
 var contCompetenciasId = 1;
 var contCertificacoesId = 1;
+var contIdiomasId = 1;
+var contIdExperiencia = 1;
 
 function addCompentecia() {
     let divCompentecias = document.getElementById("competencias");
@@ -48,3 +50,75 @@ function addCertificacao() {
 function removeCertificacao(divId) {
     document.getElementById("divCertificacao" + divId).remove();
 }
+
+function addIdioma() {
+    let divIdiomas = document.getElementById("idiomas");
+    let nomeIdioma = document.getElementById("nomeIdioma").value;
+
+    if (nomeIdioma.length < 1) {
+        return;
+    }
+
+    divIdiomas.innerHTML += `
+        <div class="input-group mb-3" id="divCertificacao${contIdiomasId}">
+            <input class="form-control" type="text" placeholder="${nomeIdioma}" aria-label="Disabled input example" disabled>
+            <button class="btn btn-outline-danger" type="button" id="button-addon2" onclick="removeCertificacao(${contIdiomasId})">Remover</button>
+        </div>
+    `;
+
+    document.getElementById("nomeIdioma").value = "";
+    contIdiomasId++;
+}
+
+function removeIdioma(divId) {
+    document.getElementById("divIdioma" + divId).remove();
+}
+
+
+
+
+function addExperiencia() {
+    let divExperiencias = document.getElementById("experiencias");
+    let nomeEmpresa = document.getElementById("nomeEmpresa").value;
+    let nomeCargo = document.getElementById("nomeCargo").value;
+    let nomeDataInicio = document.getElementById("nomeDataInicio").value;
+    let nomeDataFim = document.getElementById("nomeDataFim").value;
+    let nomeEndereco = document.getElementById("nomeEndereco").value;
+
+    divExperiencias.innerHTML += `
+            <div class="container-sm divExp" id="divExperiencia${contIdExperiencia}">
+                ${nomeEmpresa} <br>
+                ${nomeCargo} <br>
+                ${nomeDataInicio} - ${nomeDataFim} <br>
+                ${nomeEndereco}
+                <div class="divBtnEditAndDelete">
+                    <div class="d-grid gap-2 col-6 mx-auto divButtonCenter">
+                        <span class="material-symbols-outlined btnEditHover">
+                            edit
+                        </span>
+                    </div>
+                    <div class="d-grid gap-2 col-6 mx-auto divButtonCenter" onclick="removeExperiencia(${contIdExperiencia})">
+                        <span class="material-symbols-outlined btnDeleteHover">
+                            delete
+                        </span>
+                    </div>
+                </div>
+           </div>
+    `;
+    document.getElementById("nomeEmpresa").value = "";
+    document.getElementById("nomeCargo").value = "";
+    document.getElementById("nomeDataInicio").value = "";
+    document.getElementById("nomeDataFim").value = "";
+    document.getElementById("nomeEndereco").value = "";
+    contIdExperiencia++;
+}
+
+function removeExperiencia(divId) {
+    document.getElementById("divExperiencia" + divId).remove();
+}
+
+function addFormacao() {
+    
+}
+
+
